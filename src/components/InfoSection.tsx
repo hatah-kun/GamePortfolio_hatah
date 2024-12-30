484f2c4b5551fa7bafe3dd8296935812bf744125
hatah-kun <nicolasjohnabielp@gmail.com>
2024-12-30 18:29:20 +0800

    partial info layout

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -7,14 +7,35 @@ const InfoSection = () => {
       className={`h-screen w-full flex flex-col justify-center items-center
         border-white-2`}
     >
-      <div className="flex w-fit border-2 p-6">
-        <ProfileCard />
-        <p className="max-w-80">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-          assumenda veritatis aliquid accusantium laborum corrupti eveniet,
-          voluptatum voluptas quo id consequuntur doloribus excepturi! Quod
-          culpa ab hic asperiores. Cum, amet.
-        </p>
+      <div className="grid grid-cols-3 gap-4 w-4/6 border-2 p-6 border-white/15">
+        <div className="bg-white/10 col-span-1">
+          <ProfileCard />
+        </div>
+
+        <div className="bg-white/10 col-span-2 flex flex-col justify-center gap-4">
+          <div className="bg-white/10">
+            <p className="bg-white text-black font-bold font-Gemsbuck_regular v-clip-v1 w-fit px-5 py-3">
+              Player
+            </p>
+            <h1 className="text-6xl font-StylishBold mt-6">
+              JOHN ABIEL NICOLAS
+            </h1>
+            <p>
+              A developer dedicated to crafting immersive and interactive game
+              experiences, built on a foundation of creativity and technical
+              proficiency.
+            </p>
+          </div>
+          <div className="bg-white/10">
+            <h3>Info:</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+              assumenda veritatis aliquid accusantium laborum corrupti eveniet,
+              voluptatum voluptas quo id consequuntur doloribus excepturi! Quod
+              culpa ab hic asperiores. Cum, amet.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/ProfileCard.tsx
+++ src/components/ui/ProfileCard.tsx
@@ -4,30 +4,35 @@ export function ProfileCard() {
   return (
     <CardContainer className="inter-var">
       <CardBody
-        className="bg-gradient-to-t from-customColor-dark to-customColor-primary group/card 
-       w-auto sm:w-[30rem] h-auto rounded-xl p-6 border-customColor-primary border-2
+        className=" group/card 
+       w-auto sm:w-[30rem] h-auto p-6 border-customColor-primary border-2
        hover:shadow-2xl"
       >
         <CardItem as="h1" translateZ="60" className=" bg-black">
           Profile
         </CardItem>
 
-        <div className="flex flex-col justify-center items-center gap-4">
-          <CardItem translateZ="100">
+        <div className="flex flex-col justify-center items-center gap-4 relative overflow-hidden">
+          <CardItem>
+            <img src="/src/assets/playerCard_bg.png" alt="card background" />
+          </CardItem>
+
+          <CardItem className="absolute top-0 left-0">
             <img
-              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              height="1000"
-              width="1000"
-              className="h-60 w-60 object-cover rounded-full group-hover/card:shadow-xl"
-              alt="thumbnail"
+              src="/src/assets/playerCard_bgBorder.png"
+              alt="card background"
             />
           </CardItem>
-          <CardItem
-            translateZ="60"
-            className="px-4 py-2 rounded-xl text-center"
-          >
-            <h1 className="text-lg">John Abiel Nicolas</h1>
-            <p className="text-sm">Aspiring Game Developer</p>
+
+          <CardItem className="absolute bottom-0 left-0" translateZ="100">
+            <img
+              src="/src/assets/NICOLAS_VectorArt.png"
+              alt="card background"
+              className="group-hover/card:shadow-xl"
+            />
+          </CardItem>
+          <CardItem className="absolute bottom-0 left-0">
+            <img src="/src/assets/gradientShadow.png" alt="card background" />
           </CardItem>
         </div>
       </CardBody>
