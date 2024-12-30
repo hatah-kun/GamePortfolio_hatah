fc442bba4a58705a11345fd7289a7a1d2e421db2
Ronald Salvador <140569363+Zerudosuu@users.noreply.github.com>
2024-12-30 14:53:04 +0800

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,29 @@ import ShowcaseSection from "./components/ShowcaseSection";
 import InfoSection from "./components/InfoSection";
 import MessageForm from "./components/MessageForm";
 import FooterSection from "./components/FooterSection";
+import { Route, Routes } from "react-router-dom";
+import Game from "./pages/Game"; // Updated import path
+
+function AppBody() {
+  return (
+    <>
+      <IntroSection />
+      <ShowcaseSection />
+      <InfoSection />
+      <MessageForm />
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <NavBar />
-
-      <IntroSection />
-
-      <ShowcaseSection />
-
-      <InfoSection />
-
-      <MessageForm />
-
+      <Routes>
+        <Route path="/" element={<AppBody />} />
+        <Route path="/game/:id" element={<Game />} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
       <FooterSection />
     </>
   );
---
+++ src/data/Project.ts
@@ -0,0 +1,23 @@
+export const projectSample = [
+  {
+    Title: "kawatNumber1",
+    Description: "this is a project",
+    Image: "https://via.placeholder.com/150",
+    EngineUsed: "Unity",
+    Year: "2021",
+  },
+  {
+    Title: "kawatNumber2",
+    Description: "this is a project",
+    Image: "https://via.placeholder.com/150",
+    EngineUsed: "Unity",
+    Year: "2021",
+  },
+  {
+    Title: "kawatNumber3",
+    Description: "this is a project",
+    Image: "https://via.placeholder.com/150",
+    EngineUsed: "Unity",
+    Year: "2025",
+  },
+];
