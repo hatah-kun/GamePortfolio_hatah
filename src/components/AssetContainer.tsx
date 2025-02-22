6fb3d17b5cd74b12ea4ff48fc12a8cbfef5c608e
hatah-kun <nicolasjohnabielp@gmail.com>
2025-02-23 01:03:41 +0800

    make asset local

--- src/components/AssetContainer.tsx
+++ src/components/AssetContainer.tsx
@@ -18,87 +18,130 @@ import Codyssey_Poster from "../assets/GamePoster/Codyssey_poster.png";
 
 // trailers
 
-const PrototypeDefender_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1735907076/GAME_DEVELOPMENT_JOHN_ABIEL_NICOLAS_GAME_DEMO_VIDEO_FILE_oc64oa.mp4";
-const KeysAndCreeps_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1736085328/KeysAndCreepsTrailer_1_momcqq.mp4";
-const Guardian_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1736166484/Guardian_trailer_ex59hg.mp4";
-
-const Shinobi_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738169435/Portfolio_assets/trailers/ac15vpcimumpd5m6teze.mp4";
-
-const TwoBalls_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738229015/TwoBalls_trailer_x6bovp.mp4";
-
-const FruitIndia_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738251791/FruitIndia_Trailer_m75ab3.mp4";
-
-const DinoTheft_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738296923/DinoTheft_Trailer_lkanai.mp4";
-
-const URan_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738415330/Uran_trailer_b9jkul.mp4";
-
-const Codyssey_trailer =
-  "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738485789/Codessey_trailer_ke3h1v.mp4";
+import PrototypeDefender_trailer from "../assets/GameTrailers/GAME DEVELOPMENT_JOHN ABIEL NICOLAS_GAME DEMO VIDEO FILE.mp4";
+import KeysAndCreeps_trailer from "../assets/GameTrailers/KeysAndCreepsTrailer.mp4";
+import Guardian_trailer from "../assets/GameTrailers/GUARDIAN.mp4";
+import Shinobi_trailer from "../assets/GameTrailers/Shinobitrailer.mp4";
+import TwoBalls_trailer from "../assets/GameTrailers/TwoBalls_trailer.mp4";
+import FruitIndia_trailer from "../assets/GameTrailers/FruitIndia_Trailer.mp4";
+import DinoTheft_trailer from "../assets/GameTrailers/DinoTheft_Trailer.mp4";
+import URan_trailer from "../assets/GameTrailers/Uran_trailer.mp4";
+import Codyssey_trailer from "../assets/GameTrailers/Codessey_trailer.mp4";
 
 // screenshots
-const PrototypeDefender_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-1_eeyofm.png";
-const PrototypeDefender_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-3_svw2ig.png";
-const PrototypeDefender_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-2_whxbv0.png";
+import PrototypeDefender_ss1 from "../assets/GameScreenShot/PrototypeDefenderSS-1.png";
+import PrototypeDefender_ss2 from "../assets/GameScreenShot/PrototypeDefenderSS-2.png";
+import PrototypeDefender_ss3 from "../assets/GameScreenShot/PrototypeDefenderSS-3.png";
 
-const Guardian_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-1_hlo4b0.png";
-const Guardian_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-2_jbtyj7.png";
-const Guardian_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-3_lwkuml.png";
+import Guardian_ss1 from "../assets/GameScreenShot/GuardianSS-1.png";
+import Guardian_ss2 from "../assets/GameScreenShot/GuardianSS-2.png";
+import Guardian_ss3 from "../assets/GameScreenShot/GuardianSS-3.png";
 
-const Shinobi_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss1_moafkf.png";
-const Shinobi_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss3_bkgfym.png";
-const Shinobi_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss2_rj4b3l.png";
+import Shinobi_ss1 from "../assets/GameScreenShot/Shinobi_menu_ss1.png";
+import Shinobi_ss2 from "../assets/GameScreenShot/Shinobi_menu_ss2.png";
+import Shinobi_ss3 from "../assets/GameScreenShot/Shinobi_menu_ss3.png";
 
-const TwoBalls_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228985/Twoballs_SS3_sqlrzi.png";
-const TwoBalls_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228985/Twoballs_SS1_nnu5jb.png";
-const TwoBalls_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228986/Twoballs_SS2_dikiex.png";
+import TwoBalls_ss1 from "../assets/GameScreenShot/Twoballs_SS1.png";
+import TwoBalls_ss2 from "../assets/GameScreenShot/Twoballs_SS2.png";
+import TwoBalls_ss3 from "../assets/GameScreenShot/Twoballs_SS3.png";
 
-const FruitIndia_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS3_rqqex7.png";
-const FruitIndia_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS2_mwqb4x.png";
-const FruitIndia_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS1_ma4ch4.png";
+import FruitIndia_ss1 from "../assets/GameScreenShot/FruitIndia_SS1.png";
+import FruitIndia_ss2 from "../assets/GameScreenShot/FruitIndia_SS2.png";
+import FruitIndia_ss3 from "../assets/GameScreenShot/FruitIndia_SS3.png";
 
-const DinoTheft_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS1_kqwyeh.png";
-const DinoTheft_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS2_osvsta.png";
-const DinoTheft_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS3_lg0ux9.png";
+import DinoTheft_ss1 from "../assets/GameScreenShot/DinoTheft_SS1.png";
+import DinoTheft_ss2 from "../assets/GameScreenShot/DinoTheft_SS2.png";
+import DinoTheft_ss3 from "../assets/GameScreenShot/DinoTheft_SS3.png";
 
-const URan_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS1_lxzpod.png";
-const URan_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS2_fuspbk.png";
-const URan_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS3_wd2tss.png";
+import URan_ss1 from "../assets/GameScreenShot/Uran_SS1.png";
+import URan_ss2 from "../assets/GameScreenShot/Uran_SS2.png";
+import URan_ss3 from "../assets/GameScreenShot/Uran_SS3.png";
 
-const Codyssey_ss1 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485763/Codyssey_SS1_bvhm2b.png";
-const Codyssey_ss2 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485764/Codyssey_SS3_c8h7ax.png";
-const Codyssey_ss3 =
-  "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485763/Codyssey_SS2_aj7k3f.png";
+import Codyssey_ss1 from "../assets/GameScreenShot/Codyssey_SS1.png";
+import Codyssey_ss2 from "../assets/GameScreenShot/Codyssey_SS2.png";
+import Codyssey_ss3 from "../assets/GameScreenShot/Codyssey_SS3.png";
+
+// const PrototypeDefender_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1735907076/GAME_DEVELOPMENT_JOHN_ABIEL_NICOLAS_GAME_DEMO_VIDEO_FILE_oc64oa.mp4";
+// const KeysAndCreeps_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1736085328/KeysAndCreepsTrailer_1_momcqq.mp4";
+// const Guardian_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1736166484/Guardian_trailer_ex59hg.mp4";
+
+// const Shinobi_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738169435/Portfolio_assets/trailers/ac15vpcimumpd5m6teze.mp4";
+
+// const TwoBalls_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738229015/TwoBalls_trailer_x6bovp.mp4";
+
+// const FruitIndia_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738251791/FruitIndia_Trailer_m75ab3.mp4";
+
+// const DinoTheft_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738296923/DinoTheft_Trailer_lkanai.mp4";
+
+// const URan_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738415330/Uran_trailer_b9jkul.mp4";
+
+// const Codyssey_trailer =
+//   "https://res.cloudinary.com/dwxnjoocx/video/upload/v1738485789/Codessey_trailer_ke3h1v.mp4";
+
+// // screenshots
+// const PrototypeDefender_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-1_eeyofm.png";
+// const PrototypeDefender_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-3_svw2ig.png";
+// const PrototypeDefender_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1735964179/PrototypeDefenderSS-2_whxbv0.png";
+
+// const Guardian_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-1_hlo4b0.png";
+// const Guardian_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-2_jbtyj7.png";
+// const Guardian_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738223150/GuardianSS-3_lwkuml.png";
+
+// const Shinobi_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss1_moafkf.png";
+// const Shinobi_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss3_bkgfym.png";
+// const Shinobi_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738170403/Shinobi_menu_ss2_rj4b3l.png";
+
+// const TwoBalls_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228985/Twoballs_SS3_sqlrzi.png";
+// const TwoBalls_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228985/Twoballs_SS1_nnu5jb.png";
+// const TwoBalls_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738228986/Twoballs_SS2_dikiex.png";
+
+// const FruitIndia_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS3_rqqex7.png";
+// const FruitIndia_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS2_mwqb4x.png";
+// const FruitIndia_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738251769/FruitIndia_SS1_ma4ch4.png";
+
+// const DinoTheft_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS1_kqwyeh.png";
+// const DinoTheft_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS2_osvsta.png";
+// const DinoTheft_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738296892/DinoTheft_SS3_lg0ux9.png";
+
+// const URan_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS1_lxzpod.png";
+// const URan_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS2_fuspbk.png";
+// const URan_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738415297/Uran_SS3_wd2tss.png";
+
+// const Codyssey_ss1 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485763/Codyssey_SS1_bvhm2b.png";
+// const Codyssey_ss2 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485764/Codyssey_SS3_c8h7ax.png";
+// const Codyssey_ss3 =
+//   "https://res.cloudinary.com/dwxnjoocx/image/upload/v1738485763/Codyssey_SS2_aj7k3f.png";
 
 const ImageAssets = {
   Other: {
